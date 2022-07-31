774d79d6e1d00d17b2770c6095543bd325e1c3fa
anas-lubbad <anaslubbad777@gmail.com>
2022-07-31 19:00:43 +0300

    Initial Commit

---
+++ CoreData-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Example
+//
+//  Created by anas on 31/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
